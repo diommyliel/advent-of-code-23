36bf5286a455a08c6acb4dd3d8510661613e3664
diommyliel <thomascoudurie@gmail.com>
2023-12-20 19:03:56 -0500

    part 1 go

---
+++ 1/go_part_1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// init vars
+	file_name := "/home/diommy/Documents/dev/advent-of-code-23/1/input.txt"
+
+	var l int
+	var r int
+
+	sum := 0
+
+	// init file reader
+	f, err := os.Open(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		// do something with a line
+		word := scanner.Text()
+		//fmt.Printf("line: %s\n", word)
+
+		li := 0
+		ri := len(word) - 1
+
+		for li < len(word) {
+			l, err = strconv.Atoi(string(word[li]))
+			if err == nil {
+				break
+			}
+			li = li + 1
+		}
+
+		for ri >= 0 {
+			r, err = strconv.Atoi(string(word[ri]))
+			if err == nil {
+				break
+			}
+			ri = ri - 1
+		}
+
+		ret := (l * 10) + r
+		fmt.Println(ret)
+
+		sum = sum + ret
+
+	}
+
+	fmt.Println(sum)
+
+}
